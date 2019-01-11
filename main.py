5b899d98ace0415087aef8e78d3f34451376286d
xinyizhuang <31749103+xinyizhuang@users.noreply.github.com>
2019-01-10 18:57:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get("http://www.google.com")
+#r = requests.get("http://www.google.com")
+#print(dir(r))
+#print(r.text)
+#print(r.status_code)
+r = requests.get("https://github.com/xinyizhuang/cmput404lab1/edit/master/main.py")
 print(dir(r))
-print(r.text)
-print(r.status_code)
